d475238bb34f341d038a110361d47b461d1718e2
Pilirallyapp <iliakakis@gmail.com>
2026-05-06 16:33:42 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'stagemaster-v2.2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './stagemaster-icon.png'
+];
+
+// Install Event: Saves the app files to the phone's memory
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event: Cleans up old versions of the app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serves the app from memory even when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
